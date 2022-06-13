e717df5960d5e495b64c200dcd4d64af6f340814
Saurabh <saurabh30pant@gmail.com>
2022-06-13 23:17:52 +0530

    Graph using cubic Bézier curve

--- app/src/main/java/com/aqua30/graphcompose/screen/Graph.kt
+++ app/src/main/java/com/aqua30/graphcompose/screen/Graph.kt
@@ -1,7 +1,7 @@
 package com.aqua30.graphcompose.screen
 
 import android.graphics.Paint
-import android.util.Log
+import android.graphics.PointF
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -13,7 +13,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aqua30.graphcompose.ui.theme.GraphComposeTheme
@@ -29,14 +28,7 @@ fun Graph(
     val xValues = (0..9).map { it + 1 }
     val yStep = 50
     val yValues = (0..6).map { (it + 1) * yStep }
-    val random = Random()
-    val points = (0..9).map {
-        var num = random.nextInt(350)
-        if (num <= 50)
-            num += 100
-        num.toFloat()
-    }
-    Log.e("points","$points")
+    val points = listOf(150,100,250,200,330,300,90,120,285,199)
     val paddingSpace = 16.dp
     val density = LocalDensity.current
     val textPaint = remember(density) {
@@ -46,103 +38,105 @@ fun Graph(
             textSize = density.run { 12.sp.toPx() }
         }
     }
+    val connectionPoints1 = remember {
+        mutableListOf<PointF>()
+    }
+    val connectionPoints2 = remember {
+        mutableListOf<PointF>()
+    }
+    val coordinates = remember {
+        mutableListOf<PointF>()
+    }
+
     Box(
         modifier = modifier
             .background(Color.White)
-            .padding(start = 8.dp),
+            .padding(horizontal = 8.dp, vertical = 12.dp),
         contentAlignment = Center
     ) {
         Canvas(
-            modifier = Modifier
-                .fillMaxSize(),
-            onDraw = {
-                val xAxisSpace = (size.width - paddingSpace.toPx()) / xValues.size
-                val yAxisSpace = size.height / yValues.size
-                for (i in xValues.indices) {
-                    drawContext.canvas.nativeCanvas.drawText(
-                        "${xValues[i]}",
-                        xAxisSpace * (i + 1),
-                        size.height - 50,
-                        textPaint
-                    )
-                }
-                for (i in yValues.indices) {
-                    drawContext.canvas.nativeCanvas.drawText(
-                        "${yValues[i]}",
-                        paddingSpace.toPx() / 2f,
-                        size.height - yAxisSpace * (i + 1),
-                        textPaint
-                    )
-                }
-
-                Log.e("yAxisSpace","$yAxisSpace")
-                Log.e("xAxisSpace","$xAxisSpace")
-                val stroke = Path().apply {
-                    for (i in points.indices) {
-                        val x1 = xAxisSpace * xValues[i]
-                        val y1 = size.height - (yAxisSpace * (points[i]/yStep))
-                        val x2 = xAxisSpace * (xValues.getOrNull(i + 1) ?: xValues.last())
-                        val y2 = size.height - (yAxisSpace * ((points.getOrNull(i+1)?: points.last())/yStep))
-                        Log.e("coordinates","x1 = $x1, y1 = $y1,x2 = $x2, y2 = $y2")
-                        if (i == 0)
-                            moveTo(x1, y1)
-                        quadraticBezierTo(
-                            x1, y1,
-                            (x1 + x2)/2f, (y1 + y2)/2f
-                        )
-                        quadraticBezierTo(
-                            (x1 + x2)/2f, (y1 + y2)/2f,
-                            (x1 + (3 * x2))/(4f),(y1 + (3 * y2))/(4f)
-                        )
-                    }
-                }
-
-                val fillPath = android.graphics.Path(stroke.asAndroidPath())
-                    .asComposePath()
-                    .apply {
-                        lineTo(xAxisSpace * xValues.last(), size.height - yAxisSpace)
-                        lineTo(xAxisSpace, size.height - yAxisSpace)
-                        close()
-                    }
-                drawPath(
-                    fillPath,
-                    brush = Brush.verticalGradient(
-                        listOf(
-                            Color.Cyan,
-                            Color.Transparent
-                        ),
-                        endY = size.height - yAxisSpace
-                    ),
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            val xAxisSpace = (size.width - paddingSpace.toPx()) / xValues.size
+            val yAxisSpace = size.height / yValues.size
+            for (i in xValues.indices) {
+                drawContext.canvas.nativeCanvas.drawText(
+                    "${xValues[i]}",
+                    xAxisSpace * (i + 1),
+                    size.height - 30,
+                    textPaint
                 )
-                drawPath(
-                    stroke,
-                    color = Color.Black,
-                    style = Stroke(
-                        width = 5f,
-                        cap = StrokeCap.Round
-                    )
+            }
+            for (i in yValues.indices) {
+                drawContext.canvas.nativeCanvas.drawText(
+                    "${yValues[i]}",
+                    paddingSpace.toPx() / 2f,
+                    size.height - yAxisSpace * (i + 1),
+                    textPaint
                 )
+            }
+            for (i in points.indices) {
+                val x1 = xAxisSpace * xValues[i]
+                val y1 = size.height - (yAxisSpace * (points[i]/yStep.toFloat()))
+                coordinates.add(PointF(x1,y1))
+            }
+            for (i in 1 until coordinates.size) {
+                connectionPoints1.add(PointF((coordinates[i].x + coordinates[i - 1].x) / 2, coordinates[i - 1].y))
+                connectionPoints2.add(PointF((coordinates[i].x + coordinates[i - 1].x) / 2, coordinates[i].y))
+            }
 
-                for (i in points.indices) {
-                    val x1 = xAxisSpace * xValues[i]
-                    val y1 = size.height - (yAxisSpace * (points[i]/yStep))
-                    val x2 = xAxisSpace * (xValues.getOrNull(i + 1) ?: xValues.last())
-                    val y2 = size.height - (yAxisSpace * ((points.getOrNull(i+1)?: points.last())/yStep))
-                    Log.e("coordinates","x1 = $x1, y1 = $y1,x2 = $x2, y2 = $y2")
-                    drawCircle(
-                        color = Color.Red,
-                        radius = 10f,
-                        center = Offset(x1,y1)
+            val stroke = Path().apply {
+                reset()
+                moveTo(coordinates.first().x, coordinates.first().y)
+                for (i in 0 until coordinates.size - 1) {
+                    cubicTo(
+                        connectionPoints1[i].x,connectionPoints1[i].y,
+                        connectionPoints2[i].x,connectionPoints2[i].y,
+                        coordinates[i + 1].x,coordinates[i + 1].y
                     )
                 }
             }
-        )
+
+            val fillPath = android.graphics.Path(stroke.asAndroidPath())
+                .asComposePath()
+                .apply {
+                    lineTo(xAxisSpace * xValues.last(), size.height - yAxisSpace)
+                    lineTo(xAxisSpace, size.height - yAxisSpace)
+                    close()
+                }
+            drawPath(
+                fillPath,
+                brush = Brush.verticalGradient(
+                    listOf(
+                        Color.Cyan,
+                        Color.Transparent,
+                    ),
+                    endY = size.height - yAxisSpace
+                ),
+            )
+            drawPath(
+                stroke,
+                color = Color.Black,
+                style = Stroke(
+                    width = 5f,
+                    cap = StrokeCap.Round
+                )
+            )
+
+            for (i in coordinates.indices) {
+                drawCircle(
+                    color = Color.Red,
+                    radius = 10f,
+                    center = Offset(coordinates[i].x,coordinates[i].y)
+                )
+            }
+        }
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun previewGraph() {
+fun previewCurve() {
     GraphComposeTheme {
         Graph(
             modifier = Modifier
